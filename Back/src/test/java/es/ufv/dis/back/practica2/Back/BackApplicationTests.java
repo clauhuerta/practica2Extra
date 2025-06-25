0de45a08caa9ca3c08e2c08bf425c08c2f1aba81
clauhuerta <claudiahr02@icloud.com>
2025-06-25 10:29:08 +0200

    he creado el back y el front

---
+++ Back/src/test/java/es/ufv/dis/back/practica2/Back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.back.practica2.Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
